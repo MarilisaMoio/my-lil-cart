ed8132d513a623edda61a1d590853076c23f59cd
Marilisa Moio <moiomarilisa@gmail.com>
2024-07-19 16:42:05 +0200

    fix: fix a problem with localStorage and checked checkboxes

--- src/ListItem.js
+++ src/ListItem.js
@@ -1,7 +1,7 @@
 export default function ListItem({ item, onDeleteItem, onToggleItem }){
     return (
         <li> 
-            <input type="checkbox" name="inCart" value={item.inCart} onChange={() => onToggleItem(item.id)}/>
+            <input type="checkbox" name="inCart" value={item.inCart} onChange={() => onToggleItem(item.id)} checked={item.inCart}/>
             <span>{item.quantity} {item.name}</span>
             <button onClick={() => onDeleteItem(item.id)}>❌</button>
         </li>
